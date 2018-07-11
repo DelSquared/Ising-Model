26fea1e795b998465e7049110ac1715cd5655c0f
DelSquared <37172200+DelSquared@users.noreply.github.com>
2018-07-11 21:04:17 +0200

    Update main.cpp

--- Noninteracting Spins/CPP-GLFW Implementation/main.cpp
+++ Noninteracting Spins/CPP-GLFW Implementation/main.cpp
@@ -3,26 +3,62 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <time.h>
 
-//this code is incomplete
+//be sure to include GLFW. I used this video https://www.youtube.com/watch?v=0WrSGMuU964
 
-void square(float i, float j,float side, float offsetX, float offsetY);
+void square(float i, float j,float side, float offsetX, float offsetY); //functions for drawing
 void grid(float spacing,int number);
 
 int main(void)
 {
-    int side=30;
-    int windowSize = (0.5+(float)sqrt(side)/150)*side*side;
+    int side=30; 
+    int windowSize = (0.5+(float)sqrt(side)/120)*side*side;
+    float spacing = (float)side/windowSize; //setting up canvas
+    
+    srand(time(NULL));
+    //Randomising seed a bit better using time(NULL).
+    //Change to a fixed number to generate the same lattice each run
 
-    float spacing = (float)side/windowSize;
+    //grid sides [HC]
+    int S [side][side]; //setting up square spin lattice
+
+    int up=0, down=0; //Total up and down spins
+    int M=0; //Magnetisation
+    float Ebar=0,T=5,B=1,E=0; //Average energy per site, Temperature[UI], External field[UI], and total Energy
+    double Z=0; //Partition function
+
+    printf("Input T: ");
+    scanf("%f",&T);
+    printf("Input B: ");
+    scanf("%f",&B);
+
+    for (int i=0;i<side;i++){
+        for (int j=0;j<side;j++){
+            S[i][j]=2*(rand()%2)-1; //Setting up random spins as -1 or +1
+            if (S[i][j]==1) up++;
+            else down++; //Counting spins
+        };
+    };
+    Z=(double)up*exp(B/T)+down*exp(-B/T); //Z=sum(exp(-E[i][j]/kT)) and E[i][j]=-B*S[i][j]
+    M=up-down; //Determining M
+    E=abs((float)M*B); //Determining E
+    Ebar=(float)E/((float)side*(float)side); //Determining <E>
+    printf("\nUp:\t%d\tDown:\t%d\n\n",up,down);
+    printf("net M:\t%d\n",M);
+    printf("Z:\t%f\n",Z);
+    printf("E:\t%f\n",E);
+    printf("<E>:\t%f\n",(-B*up*exp(B/T)+B*down*exp(-B/T))/Z);
+    printf("Ebar:\t%f\n\n",Ebar);
+    printf("\nP(U):\t%f\nP(D):\t%f\n\n",up*exp(B/T)/Z,down*exp(-B/T)/Z);
 
     GLFWwindow* window;
 
-    /* Initialize the library */
+    //Initialize the library
     if (!glfwInit())
         return -1;
 
-    /* Create a windowed mode window and its OpenGL context */
+    //Create a windowed mode window and its OpenGL context
     window = glfwCreateWindow(windowSize, windowSize, "Lattice", NULL, NULL);
     if (!window)
     {
@@ -30,38 +66,30 @@ int main(void)
         return -1;
     }
 
-    /* Make the window's context current */
+    //Make the window's context current
     glfwMakeContextCurrent(window);
 
-    /* Loop until the user closes the window */
+    //Loop until the user closes the window
     while (!glfwWindowShouldClose(window))
     {
-        int Fill[side][side];
-
-
-        /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
         grid(spacing,side);
         for (int i=0;i<side;i++){
             for (int j=0;j<side;j++){
-                Fill[i][j]=2*(rand()%2)-1; //Setting up random spins as -1 or +1
-                if (Fill[i][j]==1) square(i,j,spacing,(float)side/2,(float)side/2);
+                if (S[i][j]==1) square(i,j,spacing,(float)side/2,(float)side/2);
             };
-            //printf("\n");
         };
-
-        /* Swap front and back buffers */
+        //Swap front and back buffers
         glfwSwapBuffers(window);
 
-        /* Poll for and process events */
+        //Poll for and process events
         glfwPollEvents();
     }
-
     glfwTerminate();
     return 0;
 }
-
+//functions for canvas rendering
 void grid(float spacing,int number) {
     float side = (float)number*spacing;
 
